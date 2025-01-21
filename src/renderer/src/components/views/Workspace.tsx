b610aa9fbf452536672ce257fa1a8333bc5f8165
Alexander <alexanderweichart@fastmail.com>
2025-01-21 21:15:54 +0100

    gutter shit

--- src/renderer/src/components/views/Workspace.tsx
+++ src/renderer/src/components/views/Workspace.tsx
@@ -14,6 +14,8 @@ const viewComponents: Record<ViewName, JSX.Element> = {
   home: <div></div>
 }
 
+type GutterPosition = 'left' | 'right' | 'top' | 'bottom'
+
 const getViewComponent = (name: ViewName) => viewComponents[name] || viewComponents.home
 
 export function WorkspaceView() {
@@ -35,35 +37,64 @@ export function WorkspaceView() {
     }
   }, [activeViewId, layout, insertRootView])
 
-  const renderLayout = (layout: Layout): React.ReactNode => {
+  const renderLayout = (
+    layout: Layout,
+    parentGutterPositions: GutterPosition[] = []
+  ): React.ReactNode => {
     if ('viewId' in layout) {
-      return renderView(layout.viewId)
+      return renderView(layout.viewId, parentGutterPositions)
     }
 
     return (
       <Splitter
+        gutterClassName={
+          layout.direction === 'Horizontal' ? 'custom-gutter-horizontal' : 'custom-gutter-vertical'
+        }
         direction={layout.direction}
         initialSizes={[layout.size, 100 - layout.size]}
         onResizeFinished={(_pairIdx, newSizes) =>
           updateSplitPanel(layout.id, layout.direction, newSizes[0])
         }
       >
-        {renderLayout(layout.panels[0])}
-        {renderLayout(layout.panels[1])}
+        {renderLayout(
+          layout.panels[0],
+          layout.direction === 'Horizontal'
+            ? [...parentGutterPositions, 'right']
+            : [...parentGutterPositions, 'bottom']
+        )}
+        {renderLayout(
+          layout.panels[1],
+          layout.direction === 'Horizontal'
+            ? [...parentGutterPositions, 'left']
+            : [...parentGutterPositions, 'top']
+        )}
       </Splitter>
     )
   }
 
-  const renderView = (viewId: string) => {
+  const renderView = (viewId: string, gutterPositions: GutterPosition[]) => {
     const currentIndex = viewIndices.get(viewId) || 0
     const stack = views.get(viewId) ?? []
     const currentState = stack[currentIndex]
     const name = currentState.name
     const Icon = ViewIcons[name]
+    let padding = ''
+    if (!gutterPositions.includes('left')) {
+      padding += ' pl-[5px]'
+    }
+    if (!gutterPositions.includes('right')) {
+      padding += ' pr-[5px]'
+    }
+    if (!gutterPositions.includes('top')) {
+      padding += ' pt-[5px]'
+    }
+    if (!gutterPositions.includes('bottom')) {
+      padding += ' pb-[5px]'
+    }
 
     return (
       <ViewProvider key={viewId} viewId={viewId}>
-        <div className="flex flex-col h-full p-[5px]">
+        <div className={`flex flex-col h-full ${padding}`}>
           <Panel
             viewId={viewId}
             name={name}
